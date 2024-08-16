de2977b72060a6a479ad023513fb0c8d7c58a05c
kamiladas <58427794+kamiladas@users.noreply.github.com>
2024-08-16 13:14:28 +0200

    Update Frame_Analiser.ino
    com Selection

--- Frame_Analiser/Frame_Analiser.ino
+++ Frame_Analiser/Frame_Analiser.ino
@@ -1,47 +1,42 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
-// Definicje pinów dla HSPI
 #define HSPI_SCK 14
 #define HSPI_MISO 12
 #define HSPI_MOSI 13
-#define HSPI_SS 15  // Możesz zmienić, jeśli używasz innego pinu dla CS
+#define HSPI_SS 15
 
 SPIClass hspi(HSPI);
 long unsigned int rxId;
 unsigned char len = 0;
 unsigned char rxBuf[8];
-char msgString[128]; // Tablica do przechowywania stringa do wysyłki przez UART
+char msgString[128];
 
-#define CAN0_INT 21 // Ustawienie INT na pin 21 dla pierwszego MCP2515
-MCP_CAN CAN0(5); // Ustawienie CS na pin 5 dla pierwszego MCP2515
+#define CAN0_INT 21
+MCP_CAN CAN0(5);
 
 void setup() {
   Serial.begin(115200);
   Serial2.begin(115200, SERIAL_8N1, 17, 16);
-  // Inicjalizacja UART2 z prędkością 115200
 
-  // Inicjalizacja pierwszego MCP2515
   if (CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
-    Serial.println("Pierwszy MCP2515 zainicjalizowany pomyślnie!");
+    Serial.println("First MCP2515 initialized successfully!");
   else
-    Serial.println("Błąd inicjalizacji pierwszego MCP2515...");
+    Serial.println("Error initializing first MCP2515...");
 
-  CAN0.setMode(MCP_NORMAL); // Ustawienie trybu operacyjnego na normalny dla pierwszego MCP2515
+  CAN0.setMode(MCP_NORMAL);
 
-  pinMode(CAN0_INT, INPUT); // Konfiguracja pinu dla wejścia /INT dla pierwszego MCP2515
+  pinMode(CAN0_INT, INPUT);
 
-  Serial.println("Przykład odbierania dla MCP2515 CAN0...");
-  Serial2.println("Serial 2 zainicjowany pomyślnie");
+  Serial.println("Receiving example for MCP2515 CAN0...");
+  Serial2.println("Serial 2 initialized successfully");
 }
 
 void loop() {
-  // Sprawdzenie wiadomości na pierwszym MCP2515
   if (!digitalRead(CAN0_INT)) {
     readCANMessage(CAN0, rxId, len, rxBuf, "MCP0");
   }
   
-  // Sprawdzenie wiadomości na UART
   if (Serial.available() > 0) {
     String uartMsg = Serial.readStringUntil('\n');
     printReceivedUARTMessage(uartMsg.c_str());
@@ -49,21 +44,20 @@ void loop() {
 }
 
 void readCANMessage(MCP_CAN& can, long unsigned int& id, unsigned char& length, unsigned char* buf, const char* mcpLabel) {
-  can.readMsgBuf(&id, &length, buf); // Odczyt danych: length = długość danych, buf = bajty danych
+  can.readMsgBuf(&id, &length, buf);
   
-  // Wydrukuj szczegóły wiadomości z identyfikatorem MCP
   printCANMessage(id, length, buf, mcpLabel);
 }
 
 void printCANMessage(long unsigned int id, unsigned char length, unsigned char* buf, const char* mcpLabel) {
-  if ((id & 0x80000000) == 0x80000000) // Określenie, czy ID jest standardowe czy rozszerzone
+  if ((id & 0x80000000) == 0x80000000)
     sprintf(msgString, "T%08X%d", (id & 0x1FFFFFFF), length);
   else
     sprintf(msgString, "T%03X%d", id, length);
 
   Serial.print(msgString);
 
-  if ((id & 0x40000000) == 0x40000000) { // Określenie, czy wiadomość jest ramką zdalną.
+  if ((id & 0x40000000) == 0x40000000) {
     Serial.print("R");
   } else {
     for (byte i = 0; i < length; i++) {
@@ -76,25 +70,20 @@ void printCANMessage(long unsigned int id, unsigned char length, unsigned char* 
 }
 
 void printReceivedUARTMessage(const char* frame) {
-  // Sprawdzenie, czy ramka zaczyna się od 'T'
   if (frame[0] != 'T') {
-    // Sprawdzenie, czy ramka to "END"
     if (frame[0] == 'E') {
-      return; // Ignoruj komunikat "END"
+      return;
     } else {
       Serial.println("Invalid frame format");
       return;
     }
   }
 
-  // Wydobycie ID ramki (3 znaki w formacie 16-bitowym)
   char idStr[4] = { frame[1], frame[2], frame[3], '\0' };
   unsigned long can_id = strtoul(idStr, NULL, 16);
 
-  // Wydobycie DLC (1 znak w formacie 16-bitowym)
   unsigned int dlc = frame[4] - '0';
 
-  // Wydobycie danych (pozostałe znaki, po dwa znaki na bajt)
   char dataStr[3] = { '\0', '\0', '\0' };
   unsigned char data[8] = {0};
 
@@ -104,7 +93,6 @@ void printReceivedUARTMessage(const char* frame) {
     data[i] = (unsigned char) strtoul(dataStr, NULL, 16);
   }
 
-  // Wypisanie zdekodowanej ramki CAN
   Serial.print("CAN ID: 0x");
   Serial.print(can_id, HEX);
   Serial.print(" DLC: ");
@@ -117,16 +105,13 @@ void printReceivedUARTMessage(const char* frame) {
   }
   Serial.println();
 
-  // Zakodowanie ramki CAN i wyświetlenie jej w konsoli
+  printCANMessage(can_id, dlc, data, "UART");
 
-  printCANMessage(can_id, dlc, data, "UART");    ///single shoot EMULATOR 
-
-  // Wysyłanie wiadomości przez CAN
   byte sndStat = CAN0.sendMsgBuf(can_id, 0, dlc, data);
   if (sndStat == CAN_OK) {
     Serial.println("Message Sent Successfully!");
   } else {
     Serial.println("Error Sending Message...");
   }
-  Serial.println(); // Dodanie pustej linii po informacji o wysłaniu
+  Serial.println();
 }
